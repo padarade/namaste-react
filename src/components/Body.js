df6315f5d6b1f65d4ecbf048ccbb4df8ae01a085
Pallavi <padarade09@gmail.com>
2024-09-11 17:29:09 +0530

    React hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import restaurantsList from "../utils/restaurant.json";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  // state variable - super power varaible
+  const [listofRestaurant, setListofRestaurant] = useState(restaurantsList);
+
+
+  // Alternate way to destructure array object of useState
+  // const arr = useState(restaurantsList);
+  // const [listofRestaurant, setListofRestaurant] = arr;
+  // or;
+  // const listofRestaurant = arr[0];
+  // const setListofRestaurant = arr[1];
+
+  // normal javascript variable
+  // let listofRestaurant= [];
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      {/* <div className="search">Search</div> */}
+      <div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("listofRestaurant", listofRestaurant);
+            const filteredList = listofRestaurant.filter(
+              (res) => res.info.avgRating > 4
+            );
+            console.log("filteredList", filteredList);
+            setListofRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {restaurantsList.map((restaurant) => {
+        {listofRestaurant.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
           );
